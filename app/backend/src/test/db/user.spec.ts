e24f2c04df40e824aa24bb297a3b0ffc78bc8865
pzareba <p.zareba@avsystem.com>
2024-09-28 17:20:06 +0200

    Formatting

--- app/backend/src/test/db/user.spec.ts
+++ app/backend/src/test/db/user.spec.ts
@@ -1,26 +1,25 @@
-import { connectToMongo, disconnectFromMongo } from "@/config/database.js"
-import { User } from "@/models/user.js"
+import { connectToMongo, disconnectFromMongo } from "@/config/database.js";
+import { User } from "@/models/user.js";
 import "dotenv/config";
 
 describe("MongoDB User integration test", () => {
   beforeAll(() => {
     return connectToMongo("mongodb://admin:admin@localhost:27017");
-  })
+  });
 
   afterAll(() => {
     return disconnectFromMongo();
-  })
+  });
 
   test("Succesfully save an user to db", async () => {
     const newUser = await new User({
-        email: "dupa@gmail.com",
-        passwordHash: "bubabiba",
-        salt: "123",
+      email: "dupa@gmail.com",
+      passwordHash: "bubabiba",
+      salt: "123",
     }).save();
 
     return User.findById(newUser._id)
-      .then(user => expect(user?.email).toBe(newUser.email))
+      .then((user) => expect(user?.email).toBe(newUser.email))
       .then(() => User.findByIdAndDelete(newUser._id));
-  })
-})
-
+  });
+});
