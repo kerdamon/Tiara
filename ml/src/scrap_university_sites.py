6e7ed66ed442ca041fdff2b38abee05000cd3e61
ppjotrek <pichor@student.agh.edu.pl>
2024-09-29 05:29:17 +0200

    ml suicide

---
+++ ml/src/scrap_university_sites.py
@@ -0,0 +1,28 @@
+import requests
+
+#AGH - wyciągnąć ze strony https://rekrutacja.agh.edu.pl/kierunki-studiow/ i na tej bazie budować link do konkretnego kierunku
+
+fields_of_study_agh = ["Automatyka i Robotyka", "Automatyka Przemysłowa i Robotyka", "Budownictwo", "Ceramika", "Chemia Budowlana", "Chemia w Kryminalistyce", "Computer Physics", "Computer Science", "Cyberbezpieczeństwo", "Digital Production for Sustainable Manufacturing", "Edukacja Techniczno-Informatyczna", "Ekologiczne Źródła Energii", "Ekoprojektowanie i Cyfryzacja Technologii Materiałowych", "Ekotechnologie Cieplne", "Electronics and Tele­communications", "Elektronika", "Elektronika i Telekomunikacja", "Elektrotechnika", "Energetyka", "Energetyka Jądrowa", "Energetyka Odnawialna i Zarządzanie Energią", "Energetyka Wodorowa", "Energy and Environmental Engineering", "Fizyka Medyczna", "Fizyka Techniczna", "Geodezja i Kartografia", "Geofizyka", "Geoinformatyka", "Geoinżynieria i Górnictwo Otworowe", "Geologia Stosowana", "Geośrodowisko Miasta", "Geotermia – Technika i Technologia", "Geoturystyka", "Informatyka - Uczenie Maszynowe i Sztuczna Inteligencja", "Informatyka – Data Science", "Informatyka", "Informatyka Geoprzestrzenna", "Informatyka i Ekonometria", "Informatyka i Systemy Inteligentne", "Informatyka Medyczna", "Informatyka Społeczna", "Informatyka Stosowana", "Informatyka Techniczna", "International Management", "Inżynieria Akustyczna", "Inżynieria Bezpieczeństwa", "Inżynieria Biomedyczna", "Inżynieria Górnicza", "Inżynieria i Analiza Danych", "Inżynieria i Monitoring Środowiska", "Inżynieria i Ochrona Środowiska", "Inżynieria i Zarządzanie Procesami Przemysłowymi", "Inżynieria Kształtowania Środowiska", "Inżynieria Materiałowa", "Inżynieria Mechaniczna i Materiałowa", "Inżynieria Mechatroniczna", "Inżynieria Metali", "Inżynieria Metali Nieżelaznych", "Inżynieria Naftowa i Gazownicza", "Inżynieria Obliczeniowa", "Inżynieria Procesów Odlewniczych", "Inżynieria Procesów Przemysłowych", "Inżynieria Produkcji i Jakości", "Komputerowe Wspomaganie Procesów Inżynierskich", "Komputerowo Wspomagana Inżynieria Materiałów Budowlanych", "Kulturo­znawstwo", "Matematyka", "Materiały i Technologie dla Energetyki i Lotnictwa", "Materiały i Technologie Metali Nieżelaznych", "Mechanical Engineering", "Mechanika i Budowa Maszyn", "Mechatronic engineering with English as instruction language", "Metalcasting Engineering", "Metallurgical Engineering", "Metalurgia", "Mikro- i Nanotechnologie w Biofizyce", "Mikro­elektronika w Technice i Medycynie", "Modern Materials Design And Application", "Nanoinżynieria Materiałów", "Nowoczesne Technologie Paliwowe", "Nowoczesne Technologie w Kryminalistyce", "Odlewnictwo", "Raw Materials Value Chain", "Recykling i Metalurgia", "Remote Sensing and Geo Informatics", "Rewitalizacja Terenów Zdegrado­wanych", "Socjologia", "Technologia Chemiczna", "Technologie Przemysłu 4.0", "Teleinformatyka", "Transport w Przemyśle 4.0", "Tworzywa i Technologie Motoryzacyjne", "Zarządzanie", "Zarządzanie i Inżynieria Produkcji", "Zmiany Klimatu - Przeciwdziałanie i Adaptacja"]
+
+def get_agh_link(field_of_study):
+    field_of_study = field_of_study.replace(" ", "-")
+    field_of_study = field_of_study.replace("ł", "l")
+    field_of_study = field_of_study.replace("ó", "o")
+    field_of_study = field_of_study.replace("ę", "e")
+    field_of_study = field_of_study.replace("ą", "a")
+    field_of_study = field_of_study.replace("ś", "s")
+    field_of_study = field_of_study.replace("ć", "c")
+    field_of_study = field_of_study.replace("ż", "z")
+    field_of_study = field_of_study.replace("ź", "z")
+    field_of_study = field_of_study.replace("ń", "n")
+    return f"https://rekrutacja.agh.edu.pl/kierunki-studiow/{field_of_study.lower()}"
+
+
+
+if __name__ == "__main__":
+    for field in fields_of_study_agh:
+        link = get_agh_link(field)
+        response = requests.get(link)
+        print(link)
+        print(response.status_code)
+        
