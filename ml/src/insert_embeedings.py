5de850e6b9663bf3b4b2f1b55c23da73e08c34b8
mateuszGorczany <mateusz.g64@interia.pl>
2024-09-29 04:25:25 +0200

    another changes

--- ml/src/insert_embeedings.py
+++ ml/src/insert_embeedings.py
@@ -1,46 +1,38 @@
 from fastapi import FastAPI, Query
 from pydantic import BaseModel
-from langchain.chains import RetrievalQA
-from langchain.docstore.document import Document
+# from langchain.chains import RetrievalQA
+# from langchain.docstore.document import Document
 from sentence_transformers import SentenceTransformer
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
 import psycopg
 import numpy as np
 import uvicorn
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 from pathlib import Path
 import json
+from db import connection_factory
+from models import Major
 
-class Settings(BaseSettings):
-    db_name: str
-    db_user: str
-    db_password: str
-    db_host: str
-
-    class Config:
-        env_file = ".env"
-
-settings = Settings()
 SentenceLatentizer = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
-conn = psycopg.connect(
-    dbname=settings.db_name, 
-    user=settings.db_user, 
-    password=settings.db_password, 
-    host=settings.db_host
-)
 
-def insert_document(content):
-    embedding = SentenceLatentizer.encode(content).astype(np.float32)  # Create the embedding
-    cur = conn.cursor()
-    cur.execute("INSERT INTO documents (content, embedding) VALUES (%s, %s)", (content, embedding.tolist()))
-    conn.commit()
+def insert_major(major):
+    embedding = SentenceLatentizer.encode(major, convert_to_numpy=True).astype(np.float32)  # Create the embedding
+    with connection_factory() as conn:
+        cur = conn.cursor()
+        cur.execute("INSERT INTO Major (vector) VALUES (%s)", (embedding.tolist(),))
+        conn.commit()
 
 def main():
 
-    fields_of_study = json.loads(Path("data/input/docs.json").read_text())
+    fields_of_study = json.loads(Path("data/input/initial_input.json").read_text(encoding='utf-8'))
 
     for field_name, field in fields_of_study.items():
-        insert_document(field)
+        # field_text = json.dumps(field, ensure_ascii=False).encode('utf8')
+        # print(field_text)
+        field = "Test test test raz dwa trzy cztery."
+        print(field)
+        # insert_major(Major(**field))
+        insert_major(field)
         # doc_embedding = SentenceLatentizer.encode(field)
         # cur = conn.cursor()
         # cur.execute("INSERT INTO fields_of_study (field, embedding) VALUES (%s, %s)", (field, doc_embedding))
---
+++ ml/src/models.py
@@ -0,0 +1,47 @@
+from pydantic import BaseModel
+from typing import List
+
+class University(BaseModel):
+    id: int
+    name: str
+    degrees: List['Major']
+
+class Major(BaseModel):
+    id: int
+    majorName: str
+    studyField: str
+    studyLevel: str
+    voivodeship: str
+    studyForm: str
+    studyProfile: str
+    semesters: str
+    faculty: str
+    numberOfGraduates: int
+    employmentSalary: float
+    timeOfLookingForJob: float
+    universityId: int
+
+    def __str__(self):
+        return (
+            f"Major Name: {self.majorName}\n"
+            f"Study Field: {self.studyField}\n"
+            f"Study Level: {self.studyLevel}\n"
+            f"Voivodeship: {self.voivodeship}\n"
+            f"Study Form: {self.studyForm}\n"
+            f"Study Profile: {self.studyProfile}\n"
+            f"Semesters: {self.semesters}\n"
+            f"Faculty: {self.faculty}\n"
+            f"Number of Graduates: {self.numberOfGraduates}\n"
+            f"Employment Salary: {self.employmentSalary}\n"
+            f"Time of Looking for Job: {self.timeOfLookingForJob}\n"
+            f"University ID: {self.universityId}"
+        )
+
+class User(BaseModel):
+    id: int
+    email: str
+    passwordHash: str
+    salt: str
+
+# To resolve the forward reference
+University.update_forward_refs()
